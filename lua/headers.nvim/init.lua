0dbc8c0a068aed79defa2aa47a38e93834bba3f7
lg-epitech <laurent.gonzalez@epitech.eu>
2024-02-26 14:59:25 +0100

    [ADD] architecture

---
+++ lua/headers.nvim/init.lua
@@ -0,0 +1,14 @@
+--
+-- EPITECH PROJECT, 2024
+-- headers.nvim
+-- File description:
+-- /home/laurent/projects/headers.nvim/lua/headers.nvim/init.lua
+--
+
+local M = {
+    _NAME = "headers.nvim",
+    _VERSION = "0.0.0-dev",
+    _AUTHOR = "Laurent Gonzalez (lg-epitech)"
+}
+
+return M
